7ad4bce8c2f5d6633f2d4b98de27b47943970104
magnetar1997 <tatsuya.s.mknr@gmail.com>
2017-08-14 22:59:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+apt.get('/article-one',function(req,res)
+{
+    res.send('Article one requested and will be served here')
+});
+apt.get('/article-two',function(req,res)
+{
+    res.send('Article two requested and will be served here')
+});
+apt.get('/article-three',function(req,res)
+{
+    res.send('Article three requested and will be served here')
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
